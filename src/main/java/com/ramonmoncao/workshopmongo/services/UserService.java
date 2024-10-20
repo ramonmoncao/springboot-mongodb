f976a223cef546657388d49ccbeb46d90b4b7dcb
Ramon Monção <ramonmoncao@hotmail.com>
2024-10-20 20:42:10 -0300

    User insert

--- src/main/java/com/ramonmoncao/workshopmongo/services/UserService.java
+++ src/main/java/com/ramonmoncao/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ramonmoncao.workshopmongo.domain.User;
+import com.ramonmoncao.workshopmongo.dto.UserDTO;
 import com.ramonmoncao.workshopmongo.repository.UserRepository;
 import com.ramonmoncao.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,11 @@ public class UserService {
 	public User findbyId(String id){
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found."));
+	}
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
