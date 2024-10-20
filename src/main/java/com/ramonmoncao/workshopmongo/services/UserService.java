8976cd23dfe2a54eebbef8deff0bfb0bd92ea6fb
Ramon Monção <ramonmoncao@hotmail.com>
2024-10-20 20:32:10 -0300

    FindbyId and not found exception

--- src/main/java/com/ramonmoncao/workshopmongo/services/UserService.java
+++ src/main/java/com/ramonmoncao/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ramonmoncao.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ramonmoncao.workshopmongo.domain.User;
 import com.ramonmoncao.workshopmongo.repository.UserRepository;
+import com.ramonmoncao.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,9 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	public User findbyId(String id){
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Object not found."));
 	}
 }
---
+++ src/main/java/com/ramonmoncao/workshopmongo/services/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.ramonmoncao.workshopmongo.services.exception;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.ramonmoncao.workshopmongo.resources.exception.StandardError;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(),"Not found.",e.getMessage(),request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
