7c36daf1f8f3e1eeb7a4c9e742689bc100ead37d
Ramon Monção <ramonmoncao@hotmail.com>
2024-10-20 19:50:03 -0300

    Database instantiation

---
+++ src/main/java/com/ramonmoncao/workshopmongo/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.ramonmoncao.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.ramonmoncao.workshopmongo.domain.User;
+import com.ramonmoncao.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		User maria = new User(null, "Maria Brown", "maria@gmail.com"); 
+		User alex = new User(null, "Alex Green", "alex@gmail.com"); 
+		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
+		
+		userRepository.deleteAll();
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+
+}
