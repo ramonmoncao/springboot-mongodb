45a0e81e03f2fe29e259098bddf0fbdd9655cc70
Ramon Monção <ramonmoncao@hotmail.com>
2024-10-20 22:28:48 -0300

    Post find by id

---
+++ src/main/java/com/ramonmoncao/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.ramonmoncao.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ramonmoncao.workshopmongo.domain.Post;
+import com.ramonmoncao.workshopmongo.repository.PostRepository;
+import com.ramonmoncao.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public List<Post> findAll() {
+		return repository.findAll();
+	}
+
+	public Post findbyId(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
+	}
+
+}
