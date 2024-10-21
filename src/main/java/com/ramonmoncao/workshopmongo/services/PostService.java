805904e8efcec734c2c0d385e9a2e5cfa5d3f0b9
Ramon Monção <ramonmoncao@hotmail.com>
2024-10-20 22:53:43 -0300

    Query methods find by title

--- src/main/java/com/ramonmoncao/workshopmongo/services/PostService.java
+++ src/main/java/com/ramonmoncao/workshopmongo/services/PostService.java
@@ -24,5 +24,9 @@ public class PostService {
 		Optional<Post> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
 	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 
 }
